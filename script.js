8a55f9ace2ffac900674c80ff8385c7c858156f3
Santiagocm07 <santicm50@gmail.com>
2025-02-23 14:53:37 -0500

    Se sube el proyecto el cual esta funcionando bien en el entorno de desarrollo

---
+++ script.js
@@ -0,0 +1,380 @@
+let clientesRegistrados = [];
+let tiempoLimiteQR = 20000; // Tiempo de espera es de 20 seg
+let contadorTiempoQR;
+let intervaloVerificacion;
+
+function verificarArchivo() {
+    // Limpiar la lista de personas no pagadas
+    clientesRegistrados = [];
+
+    const inputArchivo = document.getElementById('inputArchivo');
+    const archivo = inputArchivo.files[0];
+
+    if (!archivo) {
+        alert("No se ha seleccionado ningún archivo.");
+        return;
+    }
+
+    // Leer el archivo de Excel
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        try {
+            const data = new Uint8Array(e.target.result);
+            const libro = XLSX.read(data, { type: 'array' });
+
+            // Seleccionamos la primera hoja del archivo
+            const hoja = libro.Sheets[libro.SheetNames[0]];
+
+            // Convertimos la hoja a un formato legible
+            const datos = XLSX.utils.sheet_to_json(hoja, { header: 1 });
+
+            document.getElementById('verificarPagosBtn').style.display = 'inline-block';
+
+            // Iteramos sobre las filas de datos (saltamos la fila de encabezado)
+            for (let i = 13; i < datos.length; i++) {
+                const fila = datos[i];
+
+                // Obtener los valores de las columnas relevantes
+                const nombre = fila[4]; // Columna E de Excel
+                const telefono = fila[5]; // Columna F de Excel
+
+                // Omitir la fila que tenga el teléfono que es "5555555"
+                if (telefono == "5555555") {
+                    continue;
+                }
+
+                if (nombre && telefono) {
+                    clientesRegistrados.push({
+                        nombre: nombre,
+                        telefono: telefono
+                    });
+                }
+            
+            }
+
+            // Mostrar los clientes en la modal
+            if (clientesRegistrados.length > 0) {
+                mostrarEnModal(clientesRegistrados);
+
+                // Evento para enviar mensajes cuando el usuario haga clic en el botón
+                document.getElementById('botonEnviarMensajes').addEventListener('click', function () {
+                    enviarMensajes(clientesRegistrados);
+                });
+            } else {
+                Swal.fire({
+                    icon: 'info',
+                    title: 'Sin resultados',
+                    text: 'No hay personas con facturas pendientes.'
+                });   
+            }
+
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Hubo un problema al procesar el archivo.'
+            });     
+        }
+    };
+
+    reader.readAsArrayBuffer(archivo);
+}
+
+function mostrarEnModal(clientesRegistrados) {
+    let modalContenido = '';
+
+    clientesRegistrados.forEach(persona => {
+        modalContenido += `
+            <tr>
+                <td>${persona.nombre}</td>
+                <td>${persona.telefono}</td>
+            </tr>
+        `;
+    });
+
+    document.getElementById('tablaModal').innerHTML = modalContenido;
+    document.getElementById('modalPersonas').style.display = 'block';
+    document.getElementById('botonLeerQR').style.display = 'inline-block';
+    document.getElementById('botonEscribirMensaje').style.display = 'none';
+    
+}
+
+function cerrarModal() {
+    document.getElementById('modalPersonas').style.display = 'none';
+}
+
+// Función para manejar el clic en el botón "Leer QR"
+document.getElementById('botonLeerQR').addEventListener('click', function() {
+    // Limpiar la URL del QR anterior
+    document.getElementById('codigoQR').src = '';
+    document.getElementById('tooltipQR').style.display = 'none';
+    clearTimeout(contadorTiempoQR); // Limpiar cualquier temporizador anterior
+    clearInterval(intervaloVerificacion); // Limpiar el intervalo de verificación
+    
+    // Mostrar el "spinner" mientras obtenemos el QR
+    document.getElementById('cargando').style.display = 'block';
+
+     // Agregar un parámetro único a la URL para evitar cache
+     const timestamp = new Date().getTime();
+    fetch(`http://localhost:3000/get-qrcode?timestamp=${timestamp}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Respuesta del servidor:", data);
+            if (data.qrUrl) {
+                console.log("URL del QR:", data.qrUrl);
+                // Mostrar el QR
+                document.getElementById('codigoQR').src = data.qrUrl;
+                document.getElementById('tooltipQR').style.display = 'block'; // Mostrar el contenedor
+                verificarWhatsappListo();
+                // iniciarTemporizadorQR();
+                // if (!whatsappListo) {
+                //     iniciarTemporizadorQR();
+                // }
+            } else {
+                console.error('No se recibió la URL del QR');
+                Swal.fire({
+                    icon: 'error',
+                    title: 'QR no disponible',
+                    text: 'No se recibió correctamente el QR. Por favor, genera un nuevo código QR.'
+                });
+                document.getElementById('cargando').style.display = 'none';
+            }
+        })
+        .catch(error => {
+            console.error('Error al obtener el QR:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error de conexión',
+                text: 'Hubo un problema al obtener el código QR. Verifica tu conexión y vuelve a intentarlo.'
+            });
+            document.getElementById('cargando').style.display = 'none';  // Ocultar spinner
+        });
+});
+
+
+let whatsappListo = false;
+
+// Función para verificar que WhatsApp este listo
+async function verificarWhatsappListo() {
+    // Mostrar el "spinner" cuando comience la verificación
+    document.getElementById('cargando').style.display = 'block';
+
+    intervaloVerificacion = setInterval(async () => {
+        try {
+            const response = await fetch('http://localhost:3000/whatsapp-ready');
+            const data = await response.json();
+            if (data.ready) {
+                console.log('WhatsApp Web está listo');
+                whatsappListo = true;
+
+                // Detener el temporizador si WhatsApp está listo
+                clearTimeout(contadorTiempoQR);
+                clearInterval(intervaloVerificacion);
+
+                document.getElementById('cargando').style.display = 'none';
+                // Habilitar el botón "botonEscribirMensaje" solo cuando WhatsApp esté listo
+                document.getElementById('botonEscribirMensaje').style.display = 'inline-block';
+                // Cerrar el tooltip QR una vez que WhatsApp esté listo
+                cerrarTooltipQR();
+
+                mostrarEditorMensaje();
+            } else {
+                console.log('WhatsApp Web no está listo');
+            }
+        } catch (error) {
+            console.error('Error al verificar estado de WhatsApp:', error);
+        }
+    }, 10000); // Verificar cada 10 segundos
+
+     // Iniciar el temporizador para caducidad del QR si WhatsApp no está listo
+     contadorTiempoQR = setTimeout(() => {
+        // Solo mostrar el mensaje de caducidad si WhatsApp no está listo
+        if (!whatsappListo) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Tiempo caducado',
+                text: 'El tiempo para escanear el código QR ha caducado. Por favor, genera un nuevo código QR y escanéalo lo más pronto posible.'
+            });
+
+            document.getElementById('cargando').style.display = 'none'; 
+            cerrarTooltipQR();
+            document.getElementById('botonEscribirMensaje').style.display = 'none';
+            document.getElementById('codigoQR').src = ''; // Limpiar el QR
+            // Detener la verificación de WhatsApp si el QR caduca
+            clearInterval(intervaloVerificacion);
+        }
+    }, 120000); // 2 minutos
+}
+
+// function iniciarTemporizadorQR() {
+//     // Si WhatsApp ya está listo, no ejecutar el temporizador de caducidad
+//     if (whatsappListo) {
+//         console.log('WhatsApp ya está listo, no se inicia el temporizador de caducidad.');
+//         return;
+//     }
+
+//     clearTimeout(contadorTiempoQR); // Limpiamos cualquier temporizador anterior
+//     // clearInterval(intervaloVerificacion); // Limpiar el intervalo de verificación
+
+//     // Iniciar el temporizador para caducidad del QR
+//     contadorTiempoQR = setTimeout(() => {
+//         // Solo mostrar el mensaje de caducidad si WhatsApp no está listo
+//         if (!whatsappListo) {
+//             Swal.fire({
+//                 icon: 'warning',
+//                 title: 'Tiempo caducado',
+//                 text: 'El tiempo para escanear el código QR ha caducado. Por favor, genera un nuevo código QR y escanéalo lo más pronto posible.'
+//             });
+
+//             document.getElementById('cargando').style.display = 'none'; 
+//             cerrarTooltipQR();
+//             document.getElementById('botonEscribirMensaje').style.display = 'none';
+//             document.getElementById('codigoQR').src = ''; // Limpiar el QR
+//         }
+//     }, 25000); // 25 segundos
+// }
+
+
+function cerrarTooltipQR() {
+    document.getElementById('tooltipQR').style.display = 'none';
+
+}
+
+// Función para mostrar el editor de mensaje y el botón de enviar
+function mostrarEditorMensaje() {
+
+    // Mostrar el textarea y el botón "Enviar Mensajes"
+    document.getElementById("editorMensaje").style.display = 'block';
+    document.getElementById("botonEnviarMensajes").style.display = 'inline-block';
+}
+
+function cerrarEditorMensaje() {
+    const editorMensaje = document.getElementById('editorMensaje');
+
+    // Verificar si el editor de mensaje existe
+    if (editorMensaje) {
+        editorMensaje.style.display = 'none';
+    }
+
+    document.getElementById("botonEnviarMensajes").style.display = 'none';
+}
+
+function enviarMensajes(clientesRegistrados) {
+
+    // Obtener el mensaje del textarea
+    const mensajeUsuario = document.getElementById("mensajeUsuario").value;
+
+    // Contadores para manejar el estado de envío
+    let mensajesEnviados = 0;
+    let mensajesError = 0;
+
+    // Mostrar el "loading" mientras se envían los mensajes
+    const loading = document.getElementById('cargando');
+    const mensajeElement = loading.querySelector('p');
+    if (loading && mensajeElement) {
+        loading.style.display = 'block';
+        mensajeElement.textContent = 'Enviando mensajes...';
+    }
+
+    // Deshabilitar los botones para evitar envíos múltiples
+    document.getElementById('botonEscribirMensaje').disabled = true;
+    document.getElementById('botonEnviarMensajes').disabled = true;
+
+    // Iterar sobre cada persona que tiene pagos pendientes y enviar el mensaje
+    clientesRegistrados.forEach(persona => {
+        const mensajeBase = "Estimado(a) *" + persona.nombre + "*.\n\n";
+        const mensajeFinal = mensajeBase + mensajeUsuario;  // Combina la parte fija con el mensaje del usuario
+
+        // Enviar mensaje a la persona
+        fetch('http://localhost:3000/enviar-mensaje', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ telefono: persona.telefono, mensaje: mensajeFinal })
+        })
+        .then(response => response.json())
+        .then(data => {
+            mensajesEnviados++;  // Incrementar el contador de mensajes enviados
+            console.log('Mensaje enviado a:', persona.telefono);
+
+            // Si todos los mensajes han sido enviados, ocultar el "loading" y habilitar los botones
+            if (mensajesEnviados === clientesRegistrados.length) {
+                ocultarSpinnerYBotones(true);
+
+                setTimeout(cerrarSesionWhatsapp, 5000);
+            }
+        })
+        .catch(error => {
+            mensajesError++;  // Incrementar el contador de mensajes con error
+            console.error('Error al enviar el mensaje:', error);
+
+            // Si todos los mensajes han fallado, ocultar el "loading" y habilitar los botones
+            if (mensajesError === clientesRegistrados.length) {
+                ocultarSpinnerYBotones(false);
+            }
+        });
+    });
+}
+
+function ocultarSpinnerYBotones(exito) {
+    const loading = document.getElementById('cargando');
+    if (loading) {
+        loading.style.display = 'none';  // Ocultar el spinner
+    }
+
+    // Los botones se ocultan después de enviar los mensajes
+    document.getElementById('botonLeerQR').style.display = 'none';
+    document.getElementById('botonEscribirMensaje').style.display = 'none';
+    document.getElementById('editorMensaje').style.display = 'none';
+
+    // Mostrar mensaje de éxito o error al usuario
+    if (exito) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: 'Los mensajes fueron enviados exitosamente.'
+        });
+          
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Hubo un error al enviar los mensajes.'
+        });
+          
+    }
+}
+
+function cerrarSesionWhatsapp() {
+    // Aquí se llama a la ruta para cerrar sesión
+    fetch('http://localhost:3000/cerrar-sesion', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Sesión cerrada correctamente:", data.message);
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Sesión cerrada',
+            text: 'La sesión de WhatsApp se cerró correctamente. Si no se desvincula automáticamente en tu teléfono, ve a WhatsApp y desvincula la sesión manualmente.',
+        }).then(() => {
+            //Limpiar la URL del QR y refrescar la página
+            document.getElementById('codigoQR').src = '';
+            document.getElementById('tooltipQR').style.display = 'none';
+
+            //Recargar la página
+            location.reload();
+        });  
+    })
+    .catch(error => {
+        console.error("Error al cerrar sesión:", error);
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Hubo un problema al cerrar la sesión de WhatsApp.',
+        });
+    });
+}
---
+++ server.js
@@ -0,0 +1,156 @@
+require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
+const express = require('express');
+const cors = require('cors'); // Importa CORS
+const { Client } = require('whatsapp-web.js');
+// const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');
+const app = express();
+
+const corsOptions = {
+    origin: process.env.NODE_ENV === 'production' ? 'https://tudominio.com' : 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+};
+app.use(cors(corsOptions));
+
+// Usar CORS para permitir solicitudes del frontend
+// app.use(cors());
+
+app.use(express.json());
+// Archivos estáticos
+app.use(express.static(__dirname));
+
+// Inicializamos el cliente de WhatsApp
+const client = new Client();
+
+let whatsappListo = false;
+let qrData = null; // Almacenar el último QR generado
+let qrEsperando = false; // Indica si se está esperando que el QR sea escaneado
+let qrTiempoEspera = null; // Variable para controlar el tiempo de espera del QR
+
+// Solo generamos el QR cuando el cliente haga clic en el boton de WhatsApp
+client.on('qr', (qr) => {
+    console.log('QR generado:', qr);  // Este es el QR crudo de WhatsApp Web.
+    qrData = qr;
+
+    // Si es la primera vez que se genera el QR, empezar a esperar
+    if (!qrEsperando) {
+        qrEsperando = true; // Indicamos que estamos esperando que se escanee el QR
+        // Configurar un tiempo de espera para el Qr
+        clearTimeout(qrTiempoEspera);
+        qrTiempoEspera = setTimeout(() => {
+            console.log('Tiempo de espera del QR ha expirado');
+            qrEsperando = false;
+        }, 120000); // 2 minutos
+    }
+});
+
+// Ruta para obtener el QR
+app.get('/get-qrcode', (req, res) => {
+    console.log('Solicitud recibida para obtener el QR');
+
+    // Verifica si hay un QR disponible
+    if (!qrEsperando || !qrData) {
+        return res.status(400).json({ error: 'El tiempo para escanear el código QR ha caducado. Por favor, genera un nuevo código QR y escanéalo lo más pronto posible.' });
+    }
+
+    // Generar URL base64 del QR más reciente
+    qrcode.toDataURL(qrData, (err, url) => {
+        if (err) {
+            console.error('Error al generar el QR:', err);
+            return res.status(500).json({ error: 'Error al generar el QR' });
+        }
+
+        // Enviar la URL del QR generado en base64 al frontend
+        res.json({ qrUrl: url });
+    });
+});
+
+client.on('ready', () => {
+    console.log('WhatsApp Web está listo');
+    whatsappListo = true; // Marcar que WhatsApp Web está listo
+    qrEsperando = false; // Detener la espera del QR cuando ya está listo
+});
+
+client.initialize();
+
+// Ruta para verificar si WhatsApp Web está listo
+app.get('/whatsapp-ready', (req, res) => {
+    // Verificamos solo si no estamos esperando el QR
+    if (!qrEsperando && whatsappListo) {
+        console.log('WhatsApp Web está listo:', whatsappListo);
+        return res.json({ ready: whatsappListo });
+    }
+
+    console.log('Aún esperando el QR...');
+    res.json({ ready: false });
+});
+
+// Función para formatear el número de teléfono
+function formatearTelefono(telefono) {
+
+    const telefonoString = telefono.toString();
+
+    // Elimina cualquier caracter no numérico (como guiones o espacios)
+    const telefonoFormateado = telefonoString.replace(/[^\d]/g, '');
+
+    // Verifica si ya tiene el código de país +57 al inicio
+    if (!telefonoFormateado.startsWith('57')) {
+        // Si no lo tiene, añade el código de país (+57)
+        return '57' + telefonoFormateado;  // Para Colombia
+    }
+
+    return telefonoFormateado;  // Ya tiene el código de país, lo dejamos como está
+}
+
+// Recibe los datos del frontend
+app.post('/enviar-mensaje', async (req, res) => {
+    const { telefono, mensaje } = req.body;
+
+    // Formatear el número de teléfono
+    const telefonoFormateado = formatearTelefono(telefono);
+
+    console.log(`Recibiendo mensaje para enviar a ${telefonoFormateado}: ${mensaje}`);
+
+    try {
+        // Enviar el mensaje al chat correspondiente
+        const chat = await client.getChatById(`${telefonoFormateado}@c.us`);
+        await chat.sendMessage(mensaje);
+        console.log('Mensaje enviado:', mensaje);
+        res.status(200).json({ message: 'Mensaje enviado' });
+    } catch (error) {
+        console.error('Error al enviar el mensaje:', error);
+        res.status(500).json({ error: 'Error al enviar el mensaje' });
+    }
+});
+
+// Ruta para cerrar sesión
+app.post('/cerrar-sesion', (req, res) => {
+    if (client) {
+        // Intentar cerrar la sesión y destruir el cliente
+        client.logout().then(() => {
+            console.log("Sesión cerrada exitosamente.");
+            whatsappListo = false; // Restablecer estado 'listo'
+            client.destroy(); // Asegurarse de destruir el cliente
+            console.log("Cliente destruido.");
+
+            // Reiniciar el cliente para generar nuevo QR
+            client.initialize().then(() => {
+                console.log("Cliente reiniciado correctamente.");
+                res.status(200).json({ message: 'Sesión cerrada y cliente reiniciado' });
+            }).catch(err => {
+                console.error("Error al reiniciar el cliente:", err);
+                res.status(500).json({ error: 'Error al reiniciar el cliente' });
+            });
+        }).catch(err => {
+            console.error("Error al cerrar sesión:", err);
+            res.status(500).json({ error: 'Error al cerrar sesión' });
+        });
+    } else {
+        res.status(400).json({ error: 'No se ha iniciado sesión' });
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
